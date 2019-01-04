6983edebe9e0ce3ad08caa11ffbf36c190120dd8
Michał Huzarski <michal.huzarski@computec.pl>
2019-01-04 23:29:38 +0100

    no message

--- Source/Controle.Host/Program.cs
+++ Source/Controle.Host/Program.cs
@@ -6,7 +6,7 @@ namespace Controle.Host
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Test sdasdasd asdfasdf test test");
+            Console.WriteLine("Test xxxxxxxx asdfasdf test test");
             Console.ReadKey();
         }
     }
